abc0ce3c6c758c2ed4869bc18a8cac8d5b7c4635
RychlewL <lucas.rychlewski@epitech.eu>
2025-12-27 13:00:19 -0800

    fix: missing library checks and includes

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -40,3 +40,12 @@ else()
 
     FetchContent_MakeAvailable(spdlog)
 endif()
+
+
+# gtk workaround (linux) ----
+
+find_package(PkgConfig REQUIRED)
+pkg_check_modules(GTK3 REQUIRED gtk+-3.0)
+pkg_check_modules(WEBKIT2GTK REQUIRED webkit2gtk-4.0)
+# pkg_check_modules(CURL REQUIRED libcurl)
+include_directories(${GTK3_INCLUDE_DIRS} ${WEBKIT2GTK_INCLUDE_DIRS})
