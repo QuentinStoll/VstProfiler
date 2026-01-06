79b6d737eeb8aa20911db078a245adf33be97249
RychlewL <lucas.rychlewski@epitech.eu>
2026-01-05 19:50:19 -0800

    feat: added basic json lib for configuration files

---
+++ Source/ErrorLogger.h
@@ -0,0 +1,7 @@
+//=============================================================================
+//  file: ErrorLogger.h
+//  purpose: Header file for the error/info logger to allow the Profiller
+//      plugin to log any info or weird behavior
+//=============================================================================
+
+#include <simdjson.h>
