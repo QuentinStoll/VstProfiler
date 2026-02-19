591a29d6366c01a8005a7ccd9e832e7396eb0bfb
RychlewL <lucas.rychlewski@epitech.eu>
2026-02-18 20:58:47 -0800

    feat: more extensive bash installer script enabling preset choice

--- install.sh
+++ install.sh
@@ -7,20 +7,56 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BUILD_DIR="$SCRIPT_DIR/build"
 CACHE_DIR="$SCRIPT_DIR/.cache"
+BUILD_PRESET="default"
 
 set -e
 
 usage() {
-	echo "Usage:"
-	echo "  ./install.sh           config + build (default)"
-	echo "  ./install.sh config    cmake config only"
-	echo "  ./install.sh build     cmake build only"
-	echo "  ./install.sh re    	   cache delete + remake"
+	echo "Usage: install.sh ACTION [PRESET]"
+	echo "ACTIONS"
+	echo "  all				config + build"
+	echo "  config			cmake config only"
+	echo "  build			cmake build only"
+	echo "  re				cache delete + remake"
+	echo "PRESETS"
+	echo "  default			recommended (for dev or use)"
+	echo "  release			with standard release features"
+	echo "  all-formats		builds plugin in all availlable formats"
+	echo "  dev				all debug features"
+}
+
+presets() {
+	case "$2" in
+	"" )
+		BUILD_PRESET="default"
+		;;
+	default )
+		BUILD_PRESET="default"
+		;;
+	release )
+		BUILD_PRESET="release"
+		;;
+	all-formats )
+		BUILD_PRESET="all-formats"
+		;;
+	dev )
+		BUILD_PRESET="dev"
+		;;
+	-h|--help )
+		usage
+		exit 0
+		;;
+	* )
+		echo "[ERROR] Unknown option: $2"
+		usage
+		exit 1
+		;;
+	esac
 }
 
 config() {
 	echo "[INFO] Configuring cmake"
-	cmake -S $SCRIPT_DIR -B $BUILD_DIR
+	cmake -S $SCRIPT_DIR -B $BUILD_DIR -DPRESET_NAME=$BUILD_PRESET
 	echo "[OK] cmake configured"
 }
 
@@ -31,7 +67,7 @@ build() {
 }
 
 case "$1" in
-	"" )
+	all )
 		config
 		build
 		;;
