fa314ef9fc56fb8372bab7fd95027d06f0fd3f37
RychlewL <lucas.rychlewski@epitech.eu>
2026-04-10 03:45:52 -0700

    fix: change all source format to match clang format settings

--- Source/Include/RotarySliderParameter.h
+++ Source/Include/RotarySliderParameter.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <JuceHeader.h>
 
-struct RotarySliderParameter
-{
-	juce::String paramID;
-	juce::String name;
-	juce::String suffix;
-	float minValue;
-	float maxValue;
-	float defaultValue;
+struct RotarySliderParameter {
+    juce::String paramID;
+    juce::String name;
+    juce::String suffix;
+    float minValue;
+    float maxValue;
+    float defaultValue;
 };
