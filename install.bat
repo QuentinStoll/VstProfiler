a2372ac4d9a231ab65a6264d3521d2e5cc06653e
RychlewL <lucas.rychlewski@epitech.eu>
2026-05-14 23:52:07 -0700

    fix: solving rebase conflicts

--- install.bat
+++ install.bat
@@ -8,7 +8,7 @@ setlocal enabledelayedexpansion
 
 set "SCRIPT_DIR=%~dp0"
 set "BUILD_DIR=%SCRIPT_DIR%build"
-set "CACHE_DIR=%SCRIPT_DIR%.cache\clangd"
+set "CACHE_DIR=%SCRIPT_DIR%.cache"
 set BUILD_PRESET="default"
 
 
