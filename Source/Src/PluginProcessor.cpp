e28fcb291c6ec15aabd7faa133fb378dc145fae4
Laurent ZHANG <laurent.zhang@epitech.eu>
2026-07-14 19:10:14 +0200

    fix: add Tracy dependencies to tests and fix reuses the shared logger instead of throwing when multiple processor instances exist

--- Source/Src/PluginProcessor.cpp
+++ Source/Src/PluginProcessor.cpp
@@ -63,7 +63,9 @@ ProfilerAudioProcessor::ProfilerAudioProcessor(juce::File profileDirectory,
 {
     Log::logSystemInfoOnFileStart = (bool)(UiSettings::loadHardwareInfoSetting() - 1);
     Log::LogConfig config = Log::LogConfig::fromFile((juce::File)("/home/krt/dev/EIP/VstProfiler/.config/log_settings.json"));
-    Log::Logger& logger = Log::LogRegistry::create(config.name, config);
+    if (Log::LogRegistry::find(config.name) == nullptr) {
+        Log::LogRegistry::create(config.name, config);
+    }
 
     _masterParam = _apvts.getRawParameterValue("master");
     _gainParam = _apvts.getRawParameterValue("gain");
@@ -79,9 +81,7 @@ ProfilerAudioProcessor::ProfilerAudioProcessor(juce::File profileDirectory,
     _isEqEnabledParam = _apvts.getRawParameterValue("isEqEnabled");
 }
 
-ProfilerAudioProcessor::~ProfilerAudioProcessor() {
-    Log::LogRegistry::shutdownAll();
-}
+ProfilerAudioProcessor::~ProfilerAudioProcessor() = default;
 
 //==============================================================================
 const juce::String ProfilerAudioProcessor::getName() const {
